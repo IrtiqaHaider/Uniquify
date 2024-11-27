bf420b482d48ecd2e0c46697d628fd4e1ecff766
Irtiqa Haider <irtiqahaidermalik@gmail.com>
2024-11-27 20:54:22 +0500

    Using onRender for both hostings 5

--- backend/server.js
+++ backend/server.js
@@ -272,10 +272,8 @@ const createFile = async (data, fileType) => {
   return `/uploads/${fileName}`;
 };
 
-const server = app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+const server = app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${port}`);
 });
-
-server.timeout = 0;
 
 module.exports = app;
